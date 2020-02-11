9b051fa0461fbc91db67d8739fb74bffad3eb88e
pedejeca135 <pjco90@hotmail.com>
2020-02-11 01:26:23 -0500

    espero no sea demasiado tarde

---
+++ 4Programa/obj.hpp
@@ -0,0 +1,46 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+class Vertex
+{
+	private :
+		float x,y,z;
+	public  :
+		Vertex(float x1, float y1, float z1);
+		Vertex();
+        string toString();
+};
+
+class Edge
+{
+	private:
+		Vertex vi,vf;
+	public:
+		Edge(Vertex v1, Vertex v2);
+        string toString();
+};
+
+class Face
+{
+	private:
+		vector <Edge> aristas;
+	public:
+		Face(vector<Edge> fars);
+		Face();
+        string toString();
+};
+
+class Figure
+{
+	private:
+	vector<Face> caras;
+    char *nombre;
+	public:
+		Figure(vector<Face> faces, char *figuraNombre);
+		Figure();
+
+		string toString();
+};
